72c7bdd30f4887d70938054efc7c6e30a6b7383e
Mohit Kumar <39983059+anodit@users.noreply.github.com>
2019-11-28 11:07:38 +0530

    comment added

--- db.php
+++ db.php
@@ -1,4 +1,5 @@
 <?php
+//     loginapp is a database which we have created in the phpMyAdmin.
     $connection = mysqli_connect('localhost', 'root', '', 'loginapp');
     if(!$connection){
         die("Connection Failed");
