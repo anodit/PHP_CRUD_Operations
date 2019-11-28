0127134a410f9959d4c504ee371c9f3389150467
Mohit Kumar <39983059+anodit@users.noreply.github.com>
2019-11-28 11:15:13 +0530

    title updated

--- login_create.php
+++ login_create.php
@@ -7,7 +7,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+    <title>Create Operation</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
 </head>
 
