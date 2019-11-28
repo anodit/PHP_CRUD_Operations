2faf06606c54241252de657ba9b3877009ff4a76
Mohit Kumar <39983059+anodit@users.noreply.github.com>
2019-11-28 11:12:51 +0530

    Read operation added

---
+++ login_read.php
@@ -0,0 +1,46 @@
+<?php
+    $connection = mysqli_connect('localhost', 'root', '', 'loginapp');
+    if($connection){
+        echo "Successfully connected";
+    }
+    else{
+        echo "connnection Failed";
+    }
+    
+    $query = "SELECT * FROM users";
+
+    $result = mysqli_query($connection, $query);
+
+    if(!$result){
+        die('Query Failed'.mysqli_error());
+    }
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="col-sm-6">
+            <?php
+                while($row = mysqli_fetch_assoc($result)){
+                    ?>
+            <pre>
+                       <?php
+                        print_r($row);
+                        ?>
+                    </pre>
+            <?php    
+                    
+                }
+                ?>
+        </div>
+    </div>
+</body>
+
+</html>
